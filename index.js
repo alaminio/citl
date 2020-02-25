e2e214a51a9306958e955cde5f448680a948b83b
m3alamin <m3alamin@gmail.com>
2020-02-25 11:08:34 +0600

    direct module export

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const cl = function() {
+module.exports = (function() {
   if (typeof window === "undefined") {
     return console.log;
   }
   return window.console.log;
-};
-module.exports = cl();
+})();
